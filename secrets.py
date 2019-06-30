87517c9732886b858b9965bfd75a451a834a7d33
DaAwesomeP <DaAwesomeP@users.noreply.github.com>
2019-06-30 09:20:17 -0400

    fix line endings

--- secrets.py
+++ secrets.py
@@ -4,13 +4,7 @@ ZULIP_STREAM = 'my stream'
 ZULIP_BOT_NAME = 'ack-bot'
 ZULIP_BOT_EMAIL = 'some-bot@site.zulipchat.com'
 ZULIP_API_KEY = '000000000000000000'
-ZULIP_URL = 'https://site.zulipchat.com'
+ZULIP_URL = 'https://andrew.zulipchat.com'
 SLACK_TOKEN = 'xoxb-000000000000000000000000000'
-PUBLIC_TWO_WAY  = ['stream_to_send_both_ways']
-ZULIP_PUBLIC = 'stream_to_send_public'
-
-REDIS_HOSTNAME = '127.0.0.1'
-REDIS_PORT = 6379
-REDIS_PASSWORD = ''
-SLACK_EDIT_UPDATE_ZULIP_TTL = 60*60
-REDIS_PREFIX = 'zulip.slack'
+TWO_WAY = ['social']
+ZULIP_PUBLIC = 'social'
