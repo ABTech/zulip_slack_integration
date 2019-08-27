85f8e3b6820bfc755607875f5c57f87c5ba9e14a
DaAwesomeP <DaAwesomeP@users.noreply.github.com>
2019-08-27 15:55:09 -0400

    forward bot messages, formatting updates

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import asyncio
+from hashlib import blake2b
 import logging
 import os
 import re
@@ -11,10 +12,10 @@ import slack
 import zulip
 
 from secrets import (PUBLIC_TWO_WAY, ZULIP_BOT_NAME, ZULIP_BOT_EMAIL,
-                      ZULIP_API_KEY, ZULIP_URL, ZULIP_STREAM, ZULIP_PUBLIC,
-                      SLACK_BOT_ID, SLACK_TOKEN, REDIS_HOSTNAME, REDIS_PORT,
-                      REDIS_PASSWORD, SLACK_EDIT_UPDATE_ZULIP_TTL,
-                      REDIS_PREFIX, SLACK_ERR_CHANNEL)
+                     ZULIP_API_KEY, ZULIP_URL, ZULIP_STREAM, ZULIP_PUBLIC,
+                     SLACK_BOT_ID, SLACK_TOKEN, REDIS_HOSTNAME, REDIS_PORT,
+                     REDIS_PASSWORD, SLACK_EDIT_UPDATE_ZULIP_TTL,
+                     REDIS_PREFIX, SLACK_ERR_CHANNEL)
 
 REDIS_USERS = REDIS_PREFIX + ':users:'
 REDIS_BOTS = REDIS_PREFIX + ':bots:'
@@ -54,13 +55,13 @@ class SlackHandler(logging.StreamHandler):
         except Exception as e:
             print('could not post err to slack %s', repr(e))
 
-class ZulipSlack():
+class SlackBridge():
     def __init__(self):
-        _LOGGER.debug('new ZulipSlack instance')
+        _LOGGER.debug('new SlackBridge instance')
 
         slack_user_match = re.compile("<@[A-Z0-9]+>")
         slack_notif_match = re.compile("<![a-zA-Z0-9]+>")
-        slack_channel_match = re.compile("<#[a-zA-Z0-9]+\|[a-zA-Z0-9]+>")
+        slack_channel_match = re.compile("<#[a-zA-Z0-9]+\\|[a-zA-Z0-9]+>")
 
         _LOGGER.debug('connecting to redis')
         self.redis = redis.Redis(
@@ -112,7 +113,7 @@ class ZulipSlack():
                     data.update(data['previous_message'])
                     delete = True
                 if ('subtype' in data and
-                    data['subtype'] == 'message_replied'):
+                        data['subtype'] == 'message_replied'):
                     return
                 if not bot:
                     user_id = data['user']
@@ -123,7 +124,7 @@ class ZulipSlack():
                 if not user:
                     return
                 channel = await self.get_slack_channel(channel_id,
-                                                 web_client=web_client)
+                                                       web_client=web_client)
                 if not channel:
                     return
                 at_shift = 0
@@ -132,7 +133,7 @@ class ZulipSlack():
                     at_user_id = match[2:-1]
                     try:
                         at_user = await self.get_slack_user(at_user_id,
-                                                      web_client=web_client)
+                                                            web_client=web_client)
                         if at_user:
                             old_text = data['text']
                             start = m.start() + at_shift
@@ -142,7 +143,7 @@ class ZulipSlack():
                             at_shift = len(data['text']) - len(old_text)
                         else:
                             _LOGGER.info("couldn't find get @ user %s:",
-                                        at_user_id)
+                                         at_user_id)
                     except:
                         e = sys.exc_info()
                         exc_type, exc_value, exc_traceback = e
@@ -173,21 +174,33 @@ class ZulipSlack():
                     channel_shift = len(data['text']) - len(old_text)
 
                 if (channel['type'] == 'channel' or
-                    channel['type'] == 'private-channel'):
+                        channel['type'] == 'private-channel'):
                     msg = data['text']
                     channel_name = channel['name']
                     if ('subtype' in data and
-                        data['subtype'] in GROUP_UPDATES):
+                            data['subtype'] in GROUP_UPDATES):
                         msg_id = None
                         user = None
                     elif ('subtype' in data and
-                        data['subtype'] == 'me_message'):
+                          data['subtype'] == 'me_message'):
                         msg_id = None
                         me = True
                         if 'edited' in data:
                             edit = True
                     else:
-                        msg_id = data['client_msg_id']
+                        if 'client_msg_id' in data:
+                            msg_id = data['client_msg_id']
+                        elif 'bot_id' in data:
+                            h_bot_id = data['bot_id'].encode(encoding='UTF-8')
+                            h_msg_id = f"{data['bot_id']}{data['ts']}{msg}"
+                            h_msg_id = h_msg_id.encode(encoding='UTF-8')
+                            h = blake2b(key=h_bot_id)
+                            h.update(h_msg_id)
+                            msg_id = h.hexdigest()
+                        else:
+                            _LOGGER.warning("couldn't make a msg id: %s",
+                                            trace)
+                            return
             #        if 'files' in data:
             #            for file in data['files']:
             #                web_client.files_sharedPublicURL(id=file['id'])
@@ -206,7 +219,7 @@ class ZulipSlack():
                     _LOGGER.debug('updating user display name')
                     user = await self.get_slack_user(user_id,
                                                      web_client=web_client,
-                                               force_update=True)
+                                                     force_update=True)
                     await self.slack_web_client.chat_postMessage(
                         channel=channel_id,
                         text="OK, I have updated your display name for Slack \
@@ -252,7 +265,7 @@ be annoying.",
         _LOGGER.debug('caught zulip message')
         try:
             if (msg['subject'] in PUBLIC_TWO_WAY and
-                msg['sender_short_name'] != ZULIP_BOT_NAME):
+                    msg['sender_short_name'] != ZULIP_BOT_NAME):
                 _LOGGER.debug('good to send zulip message to slack')
                 asyncio.ensure_future(
                     self.slack_web_client.chat_postMessage(
@@ -260,15 +273,15 @@ be annoying.",
                         text=('*' + msg['sender_full_name'] + "*: " +
                               msg['content']),
                         mrkdwn=True
-            #            thread_ts=thread_ts
+                        # thread_ts=thread_ts
                     ), loop=self.slack_loop)
         except:
-                e = sys.exc_info()
-                exc_type, exc_value, exc_traceback = e
-                _LOGGER.error('Error send slack message: %s',
-                              repr(traceback.format_exception(exc_type,
-                                                              exc_value,
-                                                              exc_traceback)))
+            e = sys.exc_info()
+            exc_type, exc_value, exc_traceback = e
+            _LOGGER.error('Error send slack message: %s',
+                          repr(traceback.format_exception(exc_type,
+                                                          exc_value,
+                                                          exc_traceback)))
 
     def run_zulip_listener(self):
         self.zulip_client.call_on_each_message(self.send_to_slack)
@@ -341,7 +354,7 @@ my records to use your new name when I forward messages to Zulip for you.",
         return ret_user
 
     async def get_slack_channel(self, channel_id, web_client=None,
-                          force_update=False):
+                                force_update=False):
         redis_key = REDIS_CHANNELS + channel_id
         ret_channel = self.redis.hgetall(redis_key)
         if ret_channel is None or not ret_channel or force_update:
@@ -353,35 +366,34 @@ my records to use your new name when I forward messages to Zulip for you.",
                 _LOGGER.error('could not fetch channel %s, %s', channel_id,
                               repr(res))
                 return False
+            channel = res['channel']
+            if 'is_channel' in channel and channel['is_channel']:
+                _LOGGER.debug('found channel %s', channel_id)
+                ret_channel = {
+                    'type': 'channel',
+                    'name': channel['name']
+                }
+            elif 'is_im' in channel and channel['is_im']:
+                ret_channel = {
+                    'type': 'im',
+                    'user_id': channel['user']
+                }
+            elif ('is_group' in channel and channel['is_group'] and
+                  'is_mpim' in channel and not channel['is_mpim']):
+                ret_channel = {
+                    'type': 'private-channel',
+                    'name': channel['name']
+                }
+            elif 'is_group' in channel and channel['is_group']:
+                ret_channel = {
+                    'type': 'group',
+                    'name': channel['name']
+                }
             else:
-                channel = res['channel']
-                if 'is_channel' in channel and channel['is_channel']:
-                    _LOGGER.debug('found channel %s', channel_id)
-                    ret_channel = {
-                        'type': 'channel',
-                        'name': channel['name']
-                    }
-                elif 'is_im' in channel and channel['is_im']:
-                    ret_channel = {
-                        'type': 'im',
-                        'user_id': channel['user']
-                    }
-                elif ('is_group' in channel and channel['is_group'] and
-                      'is_mpim' in channel and not channel['is_mpim']):
-                    ret_channel = {
-                        'type': 'private-channel',
-                        'name': channel['name']
-                    }
-                elif 'is_group' in channel and channel['is_group']:
-                    ret_channel = {
-                        'type': 'group',
-                        'name': channel['name']
-                    }
-                else:
-                    _LOGGER.warning('not a channel, im, or group for %s',
-                                    channel_id)
-                    return False
-                self.redis.hmset(redis_key, ret_channel)
+                _LOGGER.warning('not a channel, im, or group for %s',
+                                channel_id)
+                return False
+            self.redis.hmset(redis_key, ret_channel)
         return ret_channel
 
     # originally from https://github.com/ABTech/zulip_groupme_integration/blob/7674a3595282ce154cd24b1903a44873d729e0cc/server.py
@@ -427,11 +439,11 @@ my records to use your new name when I forward messages to Zulip for you.",
                 pass
             elif edit and not slack_id and not send_public:
                 sent = self.zulip_client.send_message({
-                        "type": 'stream',
-                        "to": to,
-                        "subject": subject,
-                        "content": user_prefix + msg + ' *(edited)*'
-                    })
+                    "type": 'stream',
+                    "to": to,
+                    "subject": subject,
+                    "content": user_prefix + msg + ' *(edited)*'
+                })
             elif delete and slack_id:
                 redis_key = REDIS_MSG_SLACK_TO_ZULIP[to] + slack_id
                 zulip_id = self.redis.get(redis_key)
@@ -469,11 +481,11 @@ my records to use your new name when I forward messages to Zulip for you.",
                 self.redis.set(redis_key, sent['id'],
                                ex=SLACK_EDIT_UPDATE_ZULIP_TTL)
         except:
-                e = sys.exc_info()
-                exc_type, exc_value, exc_traceback = e
-                _LOGGER.error('Error send zulip message: %s',
-                              repr(traceback.format_exception(exc_type,
-                                                              exc_value,
-                                                              exc_traceback)))
+            e = sys.exc_info()
+            exc_type, exc_value, exc_traceback = e
+            _LOGGER.error('Error send zulip message: %s',
+                          repr(traceback.format_exception(exc_type,
+                                                          exc_value,
+                                                          exc_traceback)))
 
-zulip_slack = ZulipSlack()
+slack_bridge = SlackBridge()
